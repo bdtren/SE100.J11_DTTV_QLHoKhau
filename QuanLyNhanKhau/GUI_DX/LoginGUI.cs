e667f8991e5a72cae9bd68f03c0608ff14fd6136
Admin <15520161@gm.uit.edu.vn>
2018-10-16 00:45:32 +0700

    Thêm DevExpress vào giao diện đồ án

---
+++ QuanLyNhanKhau/GUI_DX/LoginGUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BUS;
+using DAO;
+using DTO;
+
+namespace GUI_DX
+{
+    public partial class LoginGUI : DevExpress.XtraEditors.XtraForm
+    {
+        public LoginGUI()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            checkLogin();
+        }
+
+        private void tbUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                checkLogin();
+            }
+        }
+        private void tbPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                checkLogin();
+            }
+        }
+
+
+        protected void checkLogin()
+        {
+            if (tbUsername.Text == "" || tbPassword.Text == "")
+            {
+                MessageBox.Show(this, "Vui lòng điền đầy đủ tài khoản và mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                DataSet ds = DBConnection_MySQL.getData("SELECT * FROM TAIKHOAN WHERE TAIKHOAN='"
+                    + tbUsername.Text + "' AND MATKHAU='" + tbPassword.Text + "'");
+                if (ds == null || ds.Tables[0].Rows.Count == 0)
+                {
+                    MessageBox.Show(this, "Tài khoản hoặc mật khẩu không đúng!\n" + DBConnection_MySQL.ErrorString, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    this.Hide();
+                    var mainGUI = new MainGUI();
+                    mainGUI.Closed += (s, args) => this.Close();
+                    mainGUI.Show();
+                }
+            }
+        }
+    }
+}
+
---
+++ QuanLyNhanKhau/GUI_DX/MainGUI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.ComponentModel.DataAnnotations;
+using DevExpress.XtraBars;
+using DTO;
+
+namespace GUI_DX
+{
+    public partial class MainGUI : XtraForm
+    {
+        public MainGUI()
+        {
+            InitializeComponent();
+            this.Text = BUS.Show.name;
+
+            DataSet tables = DBConnection_MySQL.getData("show tables from dbquanlynhankhau");
+            foreach (DataRow item in tables.Tables[0].Rows)
+            {
+                cbbTables.Items.Add(item[0].ToString());
+            }
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    MySqlDataReader myReader = DBConnection_MySQL.Query_MySQL("SELECT * FROM TAIKHOAN");
+            //    if (myReader.Read())
+            //    {
+            //        //tbContent.Text = myReader.GetString(1);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message + ((DBConnection_MySQL.ErrorString != "") ? "\n\n" + DBConnection_MySQL.ErrorString : ""));
+            //}
+            //finally
+            //{
+            //    DBConnection_MySQL.closeConnection();
+            //}
+
+            //string selected = cbbTables.GetItemText(cbbTables.SelectedItem);
+            string selected = cbbTables.Text;
+            if (string.IsNullOrWhiteSpace(selected))
+            {
+                return;
+            }
+            string query = "SELECT * FROM " + selected;
+            DataSet ds = DBConnection_MySQL.getData(query);
+            dgvContent.DataSource = ds.Tables[0];
+
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = DBConnection_MySQL.getData(rtbQuery.Text);
+                dgvQueryResult.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
